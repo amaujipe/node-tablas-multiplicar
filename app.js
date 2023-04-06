69d2c1109dc5658b80a535be04c3e2b8bb12f7f6
Andres Jimenez <amaujipe@proton.me>
2023-04-06 17:43:43 -0500

    Se agrega configuracion para usar el paquete colors de npm

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { crearArchivo } = require('./helpers/crearArchivoTabla');
 const argv = require('./config/yargs');
+const colors = require('colors');
 
 const { base, hasta, listar } = argv;
 
 crearArchivo( base, hasta, listar )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado') )
     .catch( err => console.log(err) );
--- helpers/crearArchivoTabla.js
+++ helpers/crearArchivoTabla.js
@@ -3,19 +3,24 @@ const colors = require('colors');
 
 const crearArchivo = async ( base, hasta, listar) => {
     
-    let salida = '';
-    
+    let salida  = '';
     salida += '=============\n';
     salida += `Tabla del ${base}\n`;
     salida += '=============\n';
     
+    let consola = '';
+    consola += '=============\n'.green;
+    consola += `${'Tabla del'.green} ${colors.red(base)}\n`;
+    consola += '=============\n'.green;
+    
     for(let i = 1; i <= hasta; i++) {
-        salida += `${base} x ${i} = ${base * i}\n`;
+        salida  += `${base} x ${i} = ${base * i}\n`;
+        consola += `${base} ${'x'.green} ${i} ${'='.green} ${base * i}\n`;
     }
     
     if( listar ) {
         console.clear();
-        console.log(salida);
+        console.log(consola);
     }
 
     try {
